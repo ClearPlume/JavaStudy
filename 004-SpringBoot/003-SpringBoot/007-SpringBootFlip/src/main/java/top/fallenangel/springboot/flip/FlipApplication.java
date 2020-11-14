125d14460e2cacd4f5550ba5bf30085cd8a1ee27
FallenAngel <1544931543qq.com>
2020-11-14 18:42:35 +0800

    Flip 项目修复打包错误

--- 004-SpringBoot/003-SpringBoot/007-SpringBootFlip/src/main/java/top/fallenangel/springboot/flip/FlipApplication.java
+++ 004-SpringBoot/003-SpringBoot/007-SpringBootFlip/src/main/java/top/fallenangel/springboot/flip/FlipApplication.java
@@ -3,11 +3,18 @@ package top.fallenangel.springboot.flip;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("top.fallenangel.springboot.flip.model.mapper")
-public class FlipApplication {
+public class FlipApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(FlipApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(FlipApplication.class);
 	}
 }
