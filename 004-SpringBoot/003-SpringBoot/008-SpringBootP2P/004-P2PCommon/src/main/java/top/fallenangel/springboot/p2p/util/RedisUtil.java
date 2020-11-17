39cb8d5ef6163fb2d00f623cabba05f20caebb8a
FallenAngel <1544931543qq.com>
2020-11-17 09:29:38 +0800

    P2P 项目，Redis双重验证的工具类调整；首页链接调整

--- 004-SpringBoot/003-SpringBoot/008-SpringBootP2P/004-P2PCommon/src/main/java/top/fallenangel/springboot/p2p/util/RedisUtil.java
+++ 004-SpringBoot/003-SpringBoot/008-SpringBootP2P/004-P2PCommon/src/main/java/top/fallenangel/springboot/p2p/util/RedisUtil.java
@@ -14,20 +14,33 @@ public class RedisUtil {
     }
 
     /**
-     * 从Redis中获取key的value，若key不存在，则执行operator，从数据库查找key的值并存入Redis
+     * 从Redis中获取key的value，若key不存在，则执行operator，从数据库查找key的值并存入Redis，默认超时时间为6小时
      *
      * @param key      要查找/设置的key
      * @param operator key不存在时，要进行的操作
      * @return 最终的value值
      */
     public String getValueFromRedis(String key, Operator operator) {
+        return getValueFromRedis(key, operator, 6, TimeUnit.HOURS);
+    }
+
+    /**
+     * 从Redis中获取key的value，若key不存在，则执行operator，从数据库查找key的值并存入Redis
+     *
+     * @param key      要查找/设置的key
+     * @param operator key不存在时，要进行的操作
+     * @param timeout  超时时间
+     * @param unit     超时的时间单位
+     * @return 最终的value值
+     */
+    public String getValueFromRedis(String key, Operator operator, long timeout, TimeUnit unit) {
         String valueStr = redis.opsForValue().get(key);
         if (valueStr == null) {
             synchronized (this) {
                 valueStr = redis.opsForValue().get(key);
                 if (valueStr == null) {
                     valueStr = operator.operation();
-                    redis.opsForValue().set(key, valueStr, 6, TimeUnit.HOURS);
+                    redis.opsForValue().set(key, valueStr, timeout, unit);
                 }
             }
         }
