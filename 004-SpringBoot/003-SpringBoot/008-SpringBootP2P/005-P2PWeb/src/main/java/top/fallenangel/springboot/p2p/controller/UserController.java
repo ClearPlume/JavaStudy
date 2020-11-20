745358415e67388259d4dfa594df9cd9cf8723f9
FallenAngel <1544931543qq.com>
2020-11-20 18:37:18 +0800

    撤销 "P2P 项目，登录时更新上次登录时间" 提交
    This reverts commit 4e63b53a

--- 004-SpringBoot/003-SpringBoot/008-SpringBootP2P/005-P2PWeb/src/main/java/top/fallenangel/springboot/p2p/controller/UserController.java
+++ 004-SpringBoot/003-SpringBoot/008-SpringBootP2P/005-P2PWeb/src/main/java/top/fallenangel/springboot/p2p/controller/UserController.java
@@ -194,10 +194,6 @@ public class UserController {
             return Result.error("用户名或密码错误！");
         } else {
             request.getSession().setAttribute(Constants.LOGIN_USER, user);
-            new Thread(() -> {
-                user.setLastLoginTime(new Date());
-                userService.modify(user);
-            }).start();
             return Result.success();
         }
     }
