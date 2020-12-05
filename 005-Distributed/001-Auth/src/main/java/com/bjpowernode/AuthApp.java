d4eda9f1b08fdc9223f9cb932866b8213bede8b3
FallenAngel <1544931543qq.com>
2020-12-05 19:33:16 +0800

    权限管理项目：Auth
    启动类继承SpringBootServletInitializer，为打war包做准备

--- 005-Distributed/001-Auth/src/main/java/com/bjpowernode/AuthApp.java
+++ 005-Distributed/001-Auth/src/main/java/com/bjpowernode/AuthApp.java
@@ -3,6 +3,8 @@ package com.bjpowernode;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * Hello world!
@@ -10,8 +12,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.bjpowernode.model.dao")
 // @ImportResource("classpath:spring-shiro.xml")
-public class AuthApp {
-    public static void main(String[] args) {
+public class AuthApp extends SpringBootServletInitializer
+{
+    public static void main(String[] args)
+    {
         SpringApplication.run(AuthApp.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder)
+    {
+        return builder.sources(AuthApp.class);
     }
 }
