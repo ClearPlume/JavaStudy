7364b345a524c6ec187b9510e9bfb287930b8007
FallenAngel <1544931543qq.com>
2020-12-08 11:17:04 +0800

    nginx测试项目：Session
    通过SpringSession实现分布式共享会话

--- 005-Distributed/002-Session/src/main/java/top/fallenangel/session/SessionApplication.java
+++ 005-Distributed/002-Session/src/main/java/top/fallenangel/session/SessionApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 public class SessionApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
