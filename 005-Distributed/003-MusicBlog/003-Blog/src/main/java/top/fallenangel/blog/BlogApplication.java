055f48b6406d26fea2891cc491d48fc0a51b987b
FallenAngel <1544931543qq.com>
2020-12-11 09:31:32 +0800

    微服务测试项目：SpringCloud
    Hystrix 熔断器

--- 005-Distributed/003-MusicBlog/003-Blog/src/main/java/top/fallenangel/blog/BlogApplication.java
+++ 005-Distributed/003-MusicBlog/003-Blog/src/main/java/top/fallenangel/blog/BlogApplication.java
@@ -1,11 +1,9 @@
 package top.fallenangel.blog;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 
-@EnableEurekaClient
-@SpringBootApplication
+@SpringCloudApplication
 public class BlogApplication {
 
 	public static void main(String[] args) {
--- 005-Distributed/003-MusicBlog/003-Blog/src/main/java/top/fallenangel/blog/controller/BlogController.java
+++ 005-Distributed/003-MusicBlog/003-Blog/src/main/java/top/fallenangel/blog/controller/BlogController.java
@@ -1,5 +1,7 @@
 package top.fallenangel.blog.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,18 +9,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import top.falleanngel.music.service.IMusicService;
 
+import java.util.concurrent.TimeUnit;
+
 @Controller
 @RequestMapping("blog")
+@DefaultProperties(defaultFallback = "listError")
 public class BlogController {
 
     @Autowired
     private IMusicService musicService;
 
     @GetMapping("list")
+    @HystrixCommand
     public String list(Model model) {
+
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         model.addAttribute("musicList", musicService.list());
         model.addAttribute("musicView", musicService.view());
+
+        return "blog/list";
+    }
+
+    @GetMapping
+    public String listError(Model model) {
+
+        model.addAttribute("error", "服务超时，请重试");
 
         return "blog/list";
     }
